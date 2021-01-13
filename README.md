6b32f6628c5707a274b5bef11e7f30e43c5d5922
dbrizard <denis.brizard@ifsttar.fr>
2021-01-13 07:25:10 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,75 @@
+
+# Installation
+## Python module
+Put the Python module `contents.py` somewhere in your python path. Or modify the python path to tell where to find the module.
+
+## PDF manipulating tool
+Install either [CPDF]() or [PDFTK]().
+
+## DJVU tool
+Make sure you have `djvused` installed.
+
+## Shell script
+Put the Shell script `addpdfcontents.sh` in an accessible folder, such as in `~/bin/`
+
+In case you only have PDFTK, the second part of the script has to be modified to use PDFTK instead of CPDF.
+
+# Usage
+## Write the contents of the pdf/djvu file
+First, you have to manually write the contents of the pdf file you want to put bookmarks in:
+
+* one line per bookmark;
+* blank lines are ignored;
+* the page number must be at the end of the line, seperated by a whitespace (other separator possible, not tested yet);
+* **indentation is functional**: the depth of the bookmark is proportional to the number of whitespaces at the beginning of the line;
+* if you use CPDF, the caracter `"` is reserved and connot be used (see generated file `contents.bmk`).
+
+## Convert  `contents.txt` into `contents.bmk`
+The Python module `contents.py` converts the contents of the pdf file into a format enabling the addition  of bookmarks in the pdf with the chosen tool (CPDF or PDFTK). By default, it searches the `contents.txt` file and writes a `contents.bmk` file.
+
+The simplest line is therefore:
+```
+from contents import Contents
+Contents().write4CPDF()
+```
+
+See the Python module for the following options:
+
+* page number offset;
+* open bookmarks or not (CPDF);
+* debug option in case writing the `contents.bmk` file fails.
+
+## Add the bookmarks to the file
+### With CPDF:
+```
+cpdf -add-bookmarks contents.bmk in.pdf -o out.pdf
+```
+
+### With PDFTK
+First, get the metadata of the PDF file:
+```
+pdftk file.pdf dump_data > metadata.txt
+```
+
+Then, modify the metadata file by including `contents.bmk` after the line containing the keyword `NumberOfPages:`.
+
+Finally, updade the PDF metadata:
+```
+pdftk file.pdf update_info metadata.txt output newfile.pdf
+```
+
+### With DJVU files
+Use the two following commands:
+```
+djvused -e print-outline book.djvu
+djvused -s -e 'set-outline contents.bmk' book.djvu
+```
+
+# The shell scrip `addpdfcontents.sh`
+The shell scrit allows, in on line, to directly add the bookmarks in a pdf file with CPDF, provided the `contents.txt` file is written.
+```
+addpdfcontents.sh file.pdf
+```
+
+**Warning**: the script is not yet robust to some special characters in the file name.
+
---
+++ contents.py
@@ -0,0 +1,280 @@
+# -*- coding: utf-8 -*-
+"""
+Small Python module to format the table of contents for inclusion in a 
+PDF or DJVU file.
+
+# Initial contents file formating
+
+The input file 'contents.txt' must be formated this way:
+
+- number of spaces at the beginning of the line indicates the level;
+- the number at the end of the line indicates the page;
+- (blank lines are ignored);
+
+Output defaults to 'contents.bmk'.
+
+
+One liner, provided the contents is written in 'contents.txt':
+> from contents import Contents
+>  Contents().writePDFcontents()
+
+
+# Use of output contents file
+
+## DJVU files
+
+Use the two following commands:
+
+> djvused -e print-outline book.djvu
+> djvused -s -e 'set-outline contents.bmk' book.djvu
+
+
+## PDF files
+
+First, get the metadata of the PDF file:
+> pdftk file.pdf dump_data > metadata.txt
+
+Then, modify the metadata file by including contents.bmk after the line containing
+the keyword "NumberOfPages:".
+
+Finally, updade the PDf metadata:
+> pdftk file.pdf update_info metadata.txt output newfile.pdf
+
+
+
+Created on Thu Aug 20 13:20:10 2020
+
+@author: dbrizard
+"""
+
+import os
+import subprocess as sbp
+import glob
+
+import warnings
+
+
+
+class Contents(object):
+    """
+    
+    """
+    def __init__(self, fname='contents.txt', pagesep=' ', indent=' ', debug=False):
+        """
+        
+        :param str fname: file name
+        :param str pagesep: page number separator
+        :param str indent: indentation (level) separator
+        :param bool debug: print lines for debugging purpose
+        """
+        
+        with open(fname, 'r') as f:
+            text = f.readlines()
+        
+        self.text = text # [xx.strip() for xx in text]
+        self.pagesep = pagesep
+        self.indent = indent
+        
+        
+        self.detectOffset()
+        self.treatLines(debug=debug)
+    
+    
+    def detectOffset(self):
+        """Automatic detection of page number offset, written as first line
+        
+        """
+        fline = self.text[0]
+        if "offset" in fline:
+            # remove trailing \n
+            fline.rstrip()
+            ind = fline.rfind(self.pagesep)
+            self.offset = int( fline[ind+len(self.pagesep):] )
+            print("Offset detected in first line: %i"%self.offset)
+            # remove offset line
+            self.text.pop(0)
+        else:
+            self.offset = None
+    
+    
+    def arbitrateOffset(self, offset):
+        """
+        
+        :param int offset: offset given while calling write4XXX method
+        """
+        if offset is None and self.offset is not None:
+            final_offset = self.offset
+        elif offset is not None and self.offset is None:
+            final_offset = offset
+        elif offset is not None and self.offset is not None:
+            final_offset = self.offset
+        elif offset is None and self.offset is None:
+            final_offset = 0
+        
+        return final_offset        
+    
+    
+    def treatLines(self, debug=False):
+        """
+        
+        :param bool debug: print lines for debugging purpose
+        """
+        level = []
+        title = []
+        page = []
+        for ll in self.text:
+            #---Remove trailing \n---
+            lll = ll.rstrip()
+            if debug:
+                print(lll)
+                
+            if not len(lll)==0:
+                #---Count number of leading sep---
+                ni = len(lll) - len(lll.lstrip(self.indent))
+                level.append(ni+1)
+                
+                #---Get page number---
+                ind = lll.rfind(self.pagesep)
+                pagenumb = lll[ind+len(self.pagesep):]
+                page.append(int(pagenumb))
+                
+                #---Get title---
+                ttl = lll[:ind]
+                title.append(ttl.strip())
+        
+        self.level = level
+        self.page = page
+        self.title = title
+    
+    
+    def write4PDFTK(self, fname='contents.bmk', offset=0):
+        """
+        
+        :param str fname: output filename
+        :param int offset: page number offset
+        """
+        offset = self.arbitrateOffset(offset)
+        with open(fname, 'w') as f:
+            for tt, ll, pp in zip(self.title, self.level, self.page):
+                pp = pp + offset
+                f.write('BookmarkBegin\n')
+                f.write('BookmarkTitle: %s\n'%tt)
+                f.write('BookmarkLevel: %i\n'%ll)
+                f.write('BookmarkPageNumber: %i\n'%pp)
+    
+    
+    def write4CPDF(self, fname='contents.bmk', offset=0, Open=True):
+        """
+        
+        :param str fname: output filename
+        :param int offset: page number offset
+        :param bool Open: children visible or not when pdf file loaded
+        """
+        offset = self.arbitrateOffset(offset)
+        with open(fname, 'w') as f:
+            for tt, ll, pp in zip(self.title, self.level, self.page):
+                pp = pp + offset
+                if '"' in tt:
+                    print('/!\ found illegal " character in string in:')
+                    print(tt)                    
+                if Open:
+                    line = '%i "%s" %i open\n'%(ll-1, tt, pp)
+                else:
+                    line = '%i "%s" %i\n'%(ll-1, tt, pp)
+                f.write(line)
+            
+    
+    def write4DJVU(self, fname='contents.bmk', offset=0):
+        """
+        
+        :param str fname: output filename
+        :param int offset: page number offset
+        """
+        offset = self.arbitrateOffset(offset)
+        n = len(self.title)
+        with open(fname, 'w') as f:
+            f.write('(bookmarks\n')
+            for ii, (tt, ll, pp) in enumerate(zip(self.title, self.level, self.page)):
+                pp = pp + offset
+                if ii<n-1:
+                     if self.level[ii+1]==self.level[ii]:
+                         # next is the same level
+                         f.write('("%s" "#%i")\n'%(tt, pp))
+                     elif self.level[ii+1]>self.level[ii]:
+                         # there will be sublevels, keep parenthesis open
+                         f.write('("%s" "#%i"\n'%(tt, pp))
+                     elif self.level[ii+1]<self.level[ii]:
+                         # no more sublevels, close parentheses
+                         n_close = self.level[ii]-self.level[ii+1] +1
+                         f.write('("%s" "#%i"'%(tt, pp)+' )'*n_close +'\n')
+                else:
+                    n_close = ll + 1
+                    f.write('("%s" "#%i"'%(tt, pp)+' )'*n_close +'\n')
+
+
+        
+def addPDFtoc(pdffile=None):
+    """
+    
+    """
+    warnings.warn("This is not working yet..")
+    #---GET PDF FILE---
+    if pdffile is None:
+        pdflist = glob.glob("*.pdf")
+        if not len(pdflist)==0:
+            if len(pdflist)>1:
+                print("Several PDF files...")
+                print(pdflist)
+            pdffile = pdflist[0]
+            print() 
+        else:
+            print("/!\ no pdf files in the folder...")
+            
+    
+    #---GET META DATA---
+    sbp.call(['pdftk %s dump_data meta.txt'%pdffile])
+    
+    #---SUPPOSE CONTENTS.BMK IS HERE---
+    Contents().writePDFcontents()
+    with open("contents.bmk") as f:
+        cont = f.read()
+    
+    #---MODIFY META.TXT---
+    with open('meta.txt', 'r') as f:
+        meta = f.readlines()
+    
+    # find line with "NumberOfPages"
+    for ii, ll in enumerate(meta[:20]):
+        if 'NumberOfPages' in ll:
+            ind = ii
+    
+    # write new meta.txt file
+    meta.insert(ind, cont)
+    meta2 = "".join(meta)
+    # https://stackoverflow.com/questions/10507230/insert-line-at-middle-of-file-with-python
+    with open("meta2.txt", 'r') as f:
+        f.write(meta2)
+    
+    #---ADD CONTENTS TO PDF FILE---
+    sbp.call(['pdftk', pdffile, 'update_info meta2.txt output test.pdf'])
+        
+    
+        
+    
+
+if __name__=='__main__':
+    # ---TEST CONTENTS
+    C = Contents('contents4.txt', debug=True)
+    C.write4CPDF('cont_cpdf.bmk')
+    C.write4PDFTK('cont_pdftk.bmk')
+    C.write4DJVU('cont_djvu.bmk')
+    
+    C = Contents('contents.txt')
+    
+    
+    
+    # ---TEST ADDPDFTOC---
+    if False:
+        print("TTTTTTTTTTTTTT")
+        addPDFtoc() # XXX not finished at all
+    

f7e192f08ec94bbd7464713f7e407c7362e5639c
dbrizard <denis.brizard@univ-eiffel.fr>
2022-09-05 10:23:01 +0200

    Update addpdfcontents.sh

--- addpdfcontents.sh
+++ addpdfcontents.sh
@@ -3,7 +3,7 @@
 # suppose contents.txt file is here
 python3 -c "from contents import Contents; Contents().write4CPDF()"
 # Add bookmarks to the given pdf file
-cpdf -add-bookmarks contents.bmk $1 -o $1
+cpdf -add-bookmarks contents.bmk "$1" -o "$1"
 
 
 
